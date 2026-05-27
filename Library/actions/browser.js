37409ecfdc36c16ba30ba45ab8ad4aaecf6fd39c
bera-tech-ai <beratech15@gmail.com>
2026-05-27 16:39:50 +0300

    Update browser.js

--- Library/actions/browser.js
+++ Library/actions/browser.js
@@ -6,7 +6,7 @@
  */
 const axios = require('axios')
 
-const GIFTED = 'https://api.giftedtech.co.ke'
+const GIFTED = 'https://api.gifted.co.ke'
 const GIFTED_KEY = '_0u5aff45,_0l1876s8qc'
 
 const SCREENSHOT_APIS = [
