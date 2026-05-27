1924305993428e9262bd22c8cbb05b8f5b4cc685
bera-tech-ai <beratech15@gmail.com>
2026-05-27 16:42:21 +0300

    Update GIFTED API URL in aitools.js

--- Plugins/aitools.js
+++ Plugins/aitools.js
@@ -9,7 +9,7 @@ const axios = require('axios')
 const { sendButtons } = require('gifted-btns')
 const { getBtnMode } = require('../Library/actions/btnmode')
 
-const GIFTED = 'https://api.giftedtech.co.ke'
+const GIFTED = 'https://api.gifted.co.ke'
 const GIFTED_KEY = '_0u5aff45,_0l1876s8qc'
 
 const handle = {}
